8f23f15f64a7bca4aec06c21fae483442bde4543
Daniel Nichter <daniel-nichter@users.noreply.github.com>
2021-10-08 15:48:59 -0400

    Add code comments for stuff related to Joy's work

--- metrics/collector.go
+++ metrics/collector.go
@@ -12,11 +12,20 @@ import (
 	sysvar "github.com/square/blip/metrics/var"
 )
 
+// Register registers a factory that makes one or more metrics by domain name.
+// This is function is one several integration points because it allows users
+// to plug in new metric collectors. Blip calls this function in an init function
+// to register the built-in metric collectors.
+//
+// See types in the blip package for more details.
 func Register(domain string, f blip.CollectorFactory) error {
 	r.Lock()
 	defer r.Unlock()
 	_, ok := r.factory[domain]
 	if ok {
+		// @todo This should probably be ignored so users can override built-in
+		//		 factories/collectors with their own, e.g. user-provided status.global
+		//		 replaces built-in one.
 		return fmt.Errorf("%s already registered", domain)
 	}
 	r.factory[domain] = f
@@ -24,6 +33,9 @@ func Register(domain string, f blip.CollectorFactory) error {
 	return nil
 }
 
+// Make makes a metric collector for the domain using a previously registered factory.
+//
+// See types in the blip package for more details.
 func Make(domain string, args blip.CollectorFactoryArgs) (blip.Collector, error) {
 	r.Lock()
 	defer r.Unlock()
@@ -36,27 +48,39 @@ func Make(domain string, args blip.CollectorFactoryArgs) (blip.Collector, error)
 
 // --------------------------------------------------------------------------
 
+// Register built-in collectors using built-in factories.
 func init() {
-	for _, mc := range defaultCollectors {
+	for _, mc := range builtinCollectors {
 		Register(mc, f)
 	}
 }
 
-// repo holds registered blip.CollectorFactory
+// repo holds registered blip.CollectorFactory. There's a single package
+// instance below.
 type repo struct {
 	*sync.Mutex
 	factory map[string]blip.CollectorFactory
 }
 
+// Internal package instance of repo that holds all collector factories registered
+// by calls to Register, which includes the built-in factories.
 var r = &repo{
 	Mutex:   &sync.Mutex{},
 	factory: map[string]blip.CollectorFactory{},
 }
 
+// factory is the built-in factory for creating all built-in collectors.
+// There's a single package instance below. It implements blip.CollectorFactory.
 type factory struct{}
 
+var _ blip.CollectorFactory = &factory{}
+
+// Internet package instance of factory that makes all built-it collectors.
+// This factory is registered in the init func above.
 var f = factory{}
 
+// Make makes a metric collector for the domain. This is the built-in factory
+// that makes the built-in collectors: status.global, var.global, and so on.
 func (f factory) Make(domain string, args blip.CollectorFactoryArgs) (blip.Collector, error) {
 	switch domain {
 	case "status.global":
@@ -78,7 +102,9 @@ func (f factory) Make(domain string, args blip.CollectorFactoryArgs) (blip.Colle
 	return nil, fmt.Errorf("collector for domain %s not registered", domain)
 }
 
-var defaultCollectors = []string{
+// List of built-in collectors. To add one, add its domain name here, and add
+// the same domain in the switch statement above (in factory.Make).
+var builtinCollectors = []string{
 	"status.global",
 	"var.global",
 	"size.data",
