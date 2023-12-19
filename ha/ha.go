85fc1515930ddc469aa07e7a26876d98eb44073c
Prudhvi Chaitanya Dhulipalla <prudhvi@squareup.com>
2023-12-19 14:17:17 -0800

    Changes as per PR comments

--- ha/ha.go
+++ ha/ha.go
@@ -2,16 +2,44 @@
 
 package ha
 
+import (
+	"sync"
+
+	"github.com/cashapp/blip"
+)
+
 type Manager interface {
 	Standby() bool
 }
 
-type disabled struct{}
+type ManagerFactory interface {
+	Make(monitor blip.ConfigMonitor) (Manager, error)
+}
 
-var _ Manager = disabled{}
+type disabled struct {
+}
 
 func (d disabled) Standby() bool {
 	return false
 }
 
+var once sync.Once
 var Disabled = disabled{}
+
+// Internal factory disabled by default (HA not used).
+var f ManagerFactory = Disabled
+
+func Register(mf ManagerFactory) {
+	once.Do(func() {
+		f = mf
+		blip.Debug("register HA")
+	})
+}
+
+func Make(args blip.ConfigMonitor) (Manager, error) {
+	return f.Make(args)
+}
+
+func (d disabled) Make(_ blip.ConfigMonitor) (Manager, error) {
+	return Disabled, nil
+}
